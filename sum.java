c168dc903fc7ce2d1f9e6791218dbf0911a255ab
libyX <libymolxavier@gmail.com>
2017-02-07 23:09:07 -0500

    java prog

---
+++ sum.java
@@ -0,0 +1,10 @@
+
+
+public class sum {
+	public static void main(String[] args) {
+		int x =5, y= 4;
+		int sum;
+		sum = x + y;
+		System.out.println ("Sum of the two numbers is : " + sum);
+	}
+}	
